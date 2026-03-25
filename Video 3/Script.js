565a0e37573770da070e1c1966e5efafd1dcdf29
Aryan Singh <253520735+coderaryan-dev@users.noreply.github.com>
2026-03-25 19:57:23 +0530

    Lists & Tables

---
+++ Video 3/Script.js
@@ -0,0 +1,9 @@
+alert("Lists & Tables | Aryan Singh");
+
+  window.onload = function () {
+    let number = prompt("Enter your mobile number:");
+
+    if (number) {
+      alert("Your number is: " + number);
+    }
+  };
